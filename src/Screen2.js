9a2769bd54c2b285c36855a6dc49cb8e56aa356a
Abdul Hadhi <108865426+Abdulhadhi1@users.noreply.github.com>
2023-04-01 17:28:19 +0530

    Update Screen2.js

--- src/Screen2.js
+++ src/Screen2.js
@@ -1,96 +1,104 @@
-import React, { useEffect, useState } from 'react'
-import 'bootstrap/dist/css/bootstrap.min.css';
-// import BootstrapTable from 'react-bootstrap-table-next';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import { Button } from "primereact/button";
+import { DataTable } from "primereact/datatable";
+import { Column } from "primereact/column";
+import { Link } from "react-router-dom";
+import { FilterMatchMode } from "primereact/api";
+import { InputText } from "primereact/inputtext";
 
-function Screen2() {
-  const [productList, productData] = useState([]);
-  // const table =[
-  //   {
-  //       userdata : 'id', text: 'id'
-  //   },
-  //   {
-  //     userdata : 'name', text: 'Name'
-  //   },
-  //   {
-  //     userdata : 'email', text: 'id'
-  //   },
-  //   {
-  //     userdata : 'address.street', text: 'id'
-  //   },
-  //   {
-  //     userdata : 'address.city', text: 'id'
-  //   },
-  //   {
-  //     userdata : 'address.zipcode', text: 'id'
-  //   }
-  
-  // ]
+const Screen2 = () => {
+  const [data, setData] = useState([]);
+  const [searchTerm, setSearchTerm] = useState({
+    global: { value: null, matchMode: FilterMatchMode.CONTAINS },
+  });
 
- 
+  const paginatorLeft = (
+    <Button
+      type="button"
+      icon="pi pi-refresh"
+      text
+      onClick={() => window.location.reload()}
+    />
+  );
 
-  useEffect(() =>{
-    fetch('https://jsonplaceholder.typicode.com/users')
-    .then(res => res.json())
-    .then(result => productData(result))
-    .catch(error => console.log(error));
-  }, [])
+  //
+  useEffect(() => {
+    async function getleads() {
+      const response = await axios.get(
+        `https://jsonplaceholder.typicode.com/users`
+      );
+      console.log(response.data);
+      setData(response.data);
+    }
+
+    getleads();
+  }, []);
+
   return (
     <div>
-      <div class="input-group">
-  <input type="search" class="form-control rounded" placeholder="Search" aria-label="Search" aria-describedby="search-addon" />
-  <button type="button" class="btn btn-outline-primary">search</button>
-</div>
-    
-    <table class="table">
-<thead class="thead-dark">
-  <tr>
-    <th scope="col">ID</th>
-    <th scope="col">Name</th>
-    <th scope="col">Email</th>
-    <th scope="col">City</th>
- 
-  </tr>
-</thead>{
-  productList && productList.length> 0 ?
-  productList.map(user => 
+      <div>
+        <h1 className="d-flex mt-4 justify-content-center">Screen 2</h1>
+      </div>
 
-<tbody>
-  <tr>
+      
+      <div className="d-flex my-4 justify-content-center ">
+        <InputText
+          className="bg-black text-white"
+          placeholder="search name..."
+          onInput={(e) =>
+            setSearchTerm({
+              global: {
+                value: e.target.value,
+                matchMode: FilterMatchMode.CONTAINS,
+              },
+            })
+          }
+        />
+      </div>
 
-    <td>{user.id}</td>
-    <td>{user.name}</td>
-    <td>{user.email}</td>
-    <td>{user.address.city}</td>
-  
+      <div className="card">
+        <DataTable
+          value={data}
+          sortMode="multiple"
+          paginator
+          filters={searchTerm}
+          rows={6}
+          rowsPerPageOptions={[5, 10, 15, 50]}
+          tableStyle={{ minWidth: "50rem" }}
+          paginatorTemplate="RowsPerPageDropdown FirstPageLink PrevPageLink CurrentPageReport NextPageLink LastPageLink"
+          currentPageReportTemplate="{first} to {last} of {totalRecords}"
+          paginatorLeft={paginatorLeft}
+        >
+          <Column
+            field="id"
+            header="ID"
+            style={{ width: "20%" }}
+            sortable
+          ></Column>
+          <Column
+            field="name"
+            header="Name"
+            style={{ width: "20%" }}
+            sortable
+          ></Column>
+          <Column
+            field="email"
+            header="Email"
+            style={{ width: "20%" }}
+            sortable
+          ></Column>
+          <Column
+            field="address.city"
+            header="City"
+            style={{ width: "20%" }}
+            sortable
+          ></Column>
+       
+        </DataTable>
+      </div>
+    </div>
+  );
+};
 
-  </tr> 
-
- 
- 
-</tbody> 
-  )
-  :'Loading'
-}
-</table>
-
-
-<nav aria-label="...">
-<ul class="pagination">
-  <li class="page-item disabled">
-    <a class="page-link" href="#" tabindex="-1" aria-disabled="true">Previous</a>
-  </li>
-  <li class="page-item"><a class="page-link" href="#">1</a></li>
-  <li class="page-item active" aria-current="page">
-    <a class="page-link" href="#">2</a>
-  </li>
-  <li class="page-item"><a class="page-link" href="#">3</a></li>
-  <li class="page-item">
-    <a class="page-link" href="#">Next</a>
-  </li>
-</ul>
-</nav>
-  </div>
-  )
-}
-
-export default Screen2
+export default Screen2;
